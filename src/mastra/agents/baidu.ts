c2f77c2ec501f95165dbee4eecb912e10a26cce3
Ben-artist <tskwangyi@163.com>
2025-08-10 21:53:13 +0800

    重构baidu代理配置，将服务名称更改为howtocook，并更新相关命令和参数

--- src/mastra/agents/baidu.ts
+++ src/mastra/agents/baidu.ts
@@ -1,15 +1,12 @@
 import { MCPClient } from "@mastra/mcp";
 export const mcp = new MCPClient({
   servers: {
-    baiduMap: {
+    howtocook: {
       "command": "npx",
       "args": [
         "-y",
-        "@baidumap/mcp-server-baidu-map"
-      ],
-      "env": {
-        "BAIDU_MAP_API_KEY": "iYfNm4eOjzWuxEEtGOoIHcHUHQ5ZE2rA"
-      }
+        "howtocook-mcp"
+      ]
     },
   },
 });
